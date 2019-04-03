2b2a62589a7c24882ebbb6d344d0cd78701a83fb
Dereje1 <getahun.d@gmail.com>
2019-04-02 20:24:44 -0400

    Testing: unit tests on guestbook

---
+++ __tests__/guestbook.test.js
@@ -0,0 +1,32 @@
+/* eslint-env jest */
+import { shallow } from 'enzyme';
+import React from 'react';
+import Guestbook from '../pages/guestbook';
+import Nav from '../components/nav/nav';
+import Feed from '../components/feed/feed';
+
+
+describe('<Guestbook />', () => {
+  it('renders one <Nav /> component', async () => {
+    const props = await Guestbook.getInitialProps();
+    const wrapper = shallow(<Guestbook {...props} />);
+    console.log(wrapper.debug());
+    expect(wrapper.find(Nav)).toHaveLength(1);
+  });
+
+  it('<Nav /> has currentPath set to home', async () => {
+    const props = await Guestbook.getInitialProps();
+    const wrapper = shallow(<Guestbook {...props} />);
+    expect(wrapper.find(Nav).props()).toHaveProperty('currentPath', 'guestbook');
+  });
+  it('renders one <Feed /> component', async () => {
+    const props = await Guestbook.getInitialProps();
+    const wrapper = shallow(<Guestbook {...props} />);
+    expect(wrapper.find(Feed)).toHaveLength(1);
+  });
+  it('<Feed /> has posts', async () => {
+    const props = await Guestbook.getInitialProps();
+    const wrapper = shallow(<Guestbook {...props} />);
+    expect(wrapper.find(Feed).props()).toHaveProperty('messages', props.data.posts);
+  });
+});
